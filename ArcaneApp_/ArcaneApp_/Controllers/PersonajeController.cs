d7b3e58718d4fd465bba3b8e80f7a3a56938086b
pabloyanez5 <130101518+pabloyanez5@users.noreply.github.com>
2025-04-25 23:43:24 -0500

    Upload v1

---
+++ ArcaneApp_/ArcaneApp_/Controllers/PersonajeController.cs
@@ -0,0 +1,23 @@
+﻿using ArcaneApp_.Models;
+using Microsoft.AspNetCore.Mvc;
+using ArcaneApp_.Models;
+
+namespace ArcaneApp_.Controllers
+{
+    public class PersonajeController : Controller
+    {
+        static List<Personaje> personajes = new();
+
+        public IActionResult Index()
+        {
+            return View(personajes);
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Personaje p)
+        {
+            personajes.Add(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ArcaneApp_/ArcaneApp_/Models/Personaje.cs
@@ -0,0 +1,10 @@
+﻿namespace ArcaneApp_.Models
+{
+    public class Personaje
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Rango { get; set; }
+        public string Habilidad { get; set; }
+    }
+}
